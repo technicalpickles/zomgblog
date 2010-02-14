a29d1ea322abc8ab4bae5ff50725eb4826ef6670
Joshua Nichols <josh@technicalpickles.com>
2010-02-13 20:49:14 -0500

    New ec2 instance.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,3 @@
-server "ec2-75-101-251-207.compute-1.amazonaws.com", :app, :web, :db, :primary => true
+server "ec2-184-73-9-120.compute-1.amazonaws.com", :app, :web, :db, :primary => true
 
 ssh_options[:keys] = ["#{ENV['HOME']}/.sumo/keypair.pem"]
