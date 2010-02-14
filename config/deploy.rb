5ccb0b66f5676af349014de202827d4d991b185d
Joshua Nichols <josh@technicalpickles.com>
2010-02-13 23:17:27 -0500

    Switched to railsmachine host.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,1 @@
-server "ec2-184-73-9-120.compute-1.amazonaws.com", :app, :web, :db, :primary => true
-
-ssh_options[:keys] = ["#{ENV['HOME']}/.sumo/keypair.pem"]
+server "zomgblog.railsmachine.com", :app, :web, :db, :primary => true
